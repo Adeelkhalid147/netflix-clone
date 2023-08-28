f0629a464f2d8252c37cb4c7f7500dac182c300b
AdeelKhalid147 <adeelkhalid147@gmail.com>
2023-08-29 01:48:07 +0500

    save

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -3,21 +3,27 @@ import { NextApiRequest,NextApiResponse } from 'next'
 import prismadb from '@/lib/prismadb'
 
 
-export default async function handlerr(req:NextApiRequest,res:NextApiResponse){
+export default async function handler(req:NextApiRequest,res:NextApiResponse){
+    
+    try {
     if(req.method !== 'POST'){
+        // console.log("adeel: " ,req)
         return res.status(405).end()
     }
-    try {
         const { email,name,password } = req.body 
+
         const existingUser = await prismadb.user.findUnique({
             where: {
-                email,
+                email
             }
         })
         if(existingUser) {
-            return res.status(422).json({error: 'Email taken'})
+            return res.status(422).json({error: 'Email taken'});
         }
+
         const hashedPassword = await bcrypt.hash(password,12)
+
+
         const user = await prismadb.user.create({
             data:{
                 email,name,hashedPassword,image:'',emailVerified: new Date(),
@@ -25,7 +31,7 @@ export default async function handlerr(req:NextApiRequest,res:NextApiResponse){
         })
         return res.status(200).json(user)
     } catch (error) {
-        console.log(error)
+        // console.log(error)
         return res.status(400).end()
     }
 }
