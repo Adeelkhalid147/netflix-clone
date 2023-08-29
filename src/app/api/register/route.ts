21c52640533de0ec953f27d22bbe9ef8cb037dbc
AdeelKhalid147 <adeelkhalid147@gmail.com>
2023-08-30 04:35:11 +0500

    settime

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,8 +1,11 @@
+
+/* 
 import prismadb from "@/lib/prismadb";
 import bcrypt from "bcrypt";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest, res: NextResponse) {
+
 //   console.log("errrrr: ", req);
   // if(req.method !== 'POST'){
 
@@ -10,6 +13,46 @@ export async function POST(req: NextRequest, res: NextResponse) {
   // }
   try {
     const { email, name, password } = await req.json();
+
+    const existingUser = await prismadb.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (existingUser) {
+      return NextResponse.json({ error: "Email taken" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await prismadb.user.create({
+      data: {
+        email,
+        name,
+        hashedPassword,
+        image: "",
+        emailVerified: new Date(),
+      },
+    });
+    return NextResponse.json(user);
+  } catch (error) {
+    console.log("adeel :", error);
+    return NextResponse.json({ message: error });
+  }
+}
+*/
+
+
+import prismadb from "@/lib/prismadb";
+import bcrypt from "bcrypt";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest, res: NextResponse) {
+    const id = setTimeout(() => res.json(), 9000)
+
+  try {
+    const { email, name, password } = await req.json();
+    clearTimeout(id)
 
     const existingUser = await prismadb.user.findUnique({
       where: {
