dfce273d1d78975857006bfbeb287d105ce7d8dd
AdeelKhalid147 <adeelkhalid147@gmail.com>
2023-08-30 04:38:39 +0500

    returnback

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,5 +1,4 @@
 
-/* 
 import prismadb from "@/lib/prismadb";
 import bcrypt from "bcrypt";
 import { NextRequest, NextResponse } from "next/server";
@@ -13,46 +12,6 @@ export async function POST(req: NextRequest, res: NextResponse) {
   // }
   try {
     const { email, name, password } = await req.json();
-
-    const existingUser = await prismadb.user.findUnique({
-      where: {
-        email,
-      },
-    });
-    if (existingUser) {
-      return NextResponse.json({ error: "Email taken" });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 12);
-
-    const user = await prismadb.user.create({
-      data: {
-        email,
-        name,
-        hashedPassword,
-        image: "",
-        emailVerified: new Date(),
-      },
-    });
-    return NextResponse.json(user);
-  } catch (error) {
-    console.log("adeel :", error);
-    return NextResponse.json({ message: error });
-  }
-}
-*/
-
-
-import prismadb from "@/lib/prismadb";
-import bcrypt from "bcrypt";
-import { NextRequest, NextResponse } from "next/server";
-
-export async function POST(req: NextRequest, res: NextResponse) {
-    const id = setTimeout(() => res.json(), 9000)
-
-  try {
-    const { email, name, password } = await req.json();
-    clearTimeout(id)
 
     const existingUser = await prismadb.user.findUnique({
       where: {
